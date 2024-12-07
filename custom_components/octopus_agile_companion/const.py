131a2ee2988fbc85419c82b940b84d45068e5f24
wyvernp <35661956+wyvernp@users.noreply.github.com>
2024-12-07 09:13:24 +0000

    Update const.py

--- custom_components/octopus_agile_companion/const.py
+++ custom_components/octopus_agile_companion/const.py
@@ -8,7 +8,7 @@ CONF_FETCH_WINDOW_END = "fetch_window_end"
 
 DEFAULT_FETCH_WINDOW_START = "16:00"
 DEFAULT_FETCH_WINDOW_END = "20:00"
-DEFAULT_CONSECUTIVE_PERIODS = [60, 120, 180]  # Default periods in minutes
+DEFAULT_CONSECUTIVE_PERIODS = [30, 60, 120, 180]  # Default periods in minutes
 
 DATA_COORDINATOR = "coordinator"
 EXPECTED_SLOTS_PER_DAY = 48  # 48 half-hour slots in a day
